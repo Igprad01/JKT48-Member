36f5d5c8b2f806ad8fafbb50034bf57740d08420
igprad01 <igustiputra99@gmail.com>
2024-05-25 15:24:24 +0700

    add pathfolder

--- api/index.js
+++ api/index.js
@@ -1,14 +1,25 @@
-const express = require("express");
+const express = require('express');
 const app = new express
 const port = 3000
+const path = require("path");
+const fs = require('fs');
+
 
 app.listen(port, () => {
-    console.log("server running at port 3000");
+    console.log(`running ${port}`);
 });
 
 app.get("/member",(req,res,next) => {
-    // ngambil path disini
+    const filepath = path.join(__dirname,'..', 'data', 'member.json');
+    fs.readFile(filepath, 'utf8', (err, data) => {
+       if (err) {
+        next(err)
+       } else {
+        res.json(JSON.parse(data));
+       }
+    })
 });
 
-// nanti dipelajari mengenai API lebih mendalam + terkait post data 
+
+
  
