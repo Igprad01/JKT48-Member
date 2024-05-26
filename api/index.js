081375c00ea557589a83c60ff869ec2757ed12f5
igprad01 <igustiputra99@gmail.com>
2024-05-26 15:47:22 +0700

    finishing post data

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import fs, { write } from 'fs';
 const app = express();
 const port = 3000
+let json;
 
 app.use(express.json());
 
@@ -10,13 +11,10 @@ app.listen(port, () => {
     console.log(`running ${port}`);
 });
 
-app.get('/', (res) => {
-    res.send("ini adalah API");
-})
 
 app.get("/member",(req,res,next) => {
     const filepath = path.join(__dirname,'..', 'data', 'member.json');
-    fs.readFile(filepath, 'utf8',(err, data) => {
+    fs.readFile(filepath, 'utf-8',(err, data) => {
        if (err) {
         next(err)
        } else {
@@ -27,9 +25,27 @@ app.get("/member",(req,res,next) => {
 
 app.post("/post", (req,res,next) => {
     const filepath = path.join(__dirname, '..', 'data', 'member.json');
-    const NewMember = req.body;
+    const newMember = req.body;
 
     fs.readFile(filepath, 'utf8',(err, data) => {
-        // dilanjut nanti
+        if (err) {
+            return next(err)
+        }
+        try {
+            json = JSON.parse(data);
+        } catch (parseErr) {
+            return next(parseErr);
+        }
+        
+        json.push(newMember);
+
+        fs.writeFile(filepath, JSON.stringify(json, null, 2), 'utf8', (writeErr) => {
+            if (writeErr) {
+                return next(writeErr)
+            } 
+            res.status(201).json(newMember);
+        })
     })
+
+    console.log("data tersebut sudah dikirim : ");
 })
